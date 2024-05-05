e0587e2dbfe49f9657a4c03cc6255536f67bd947
Ossama9 <oussama.dahbi98@gmail.com>
2024-05-05 19:16:14 +0200

    back - ok

--- Back/auth/dto.go
+++ Back/auth/dto.go
@@ -3,7 +3,6 @@ package auth
 type UserRegistrationRequest struct {
 	Email     string `json:"email" binding:"required,email"`
 	Password  string `json:"password" binding:"required,min=8"`
-	Username  string `json:"username" binding:"required"`
 	FirstName string `json:"firstname" binding:"required"`
 	LastName  string `json:"lastname" binding:"required"`
 }
