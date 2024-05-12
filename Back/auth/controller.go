167d9897dc09dc0d0fc09b6741aa4560901f39fa
Ossama9 <oussama.dahbi98@gmail.com>
2024-05-12 15:50:15 +0200

    add auth/me

--- Back/auth/controller.go
+++ Back/auth/controller.go
@@ -1,8 +1,8 @@
 package auth
 
 import (
+	"Colibris/services"
 	"Colibris/users"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -15,21 +15,10 @@ func NewAuthController(authService Service) *Controller {
 	return &Controller{authService: authService}
 }
 
-type ApiResponse struct {
-	Success bool        `json:"success"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-	Error   string      `json:"error,omitempty"`
-}
-
 func (ctl *Controller) Register(c *gin.Context) {
 	var req UserRegistrationRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, ApiResponse{
-			Success: false,
-			Message: "Invalid request data",
-			Error:   err.Error(),
-		})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	user := users.User{
@@ -39,43 +28,41 @@ func (ctl *Controller) Register(c *gin.Context) {
 		Lastname:  req.LastName,
 	}
 	if err := ctl.authService.Register(&user); err != nil {
-		c.JSON(http.StatusInternalServerError, ApiResponse{
-			Success: false,
-			Message: "Failed to register user",
-			Error:   err.Error(),
-		})
+		c.JSON(http.StatusFailedDependency, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusCreated, ApiResponse{
-		Success: true,
-		Message: "User registered successfully",
-		Data:    user,
-	})
+	c.JSON(http.StatusCreated, user)
 }
 
 func (ctl *Controller) Login(c *gin.Context) {
 	var req UserLoginRequest
-	fmt.Print("req", req)
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, ApiResponse{
-			Success: false,
-			Message: "Invalid request data",
-			Error:   err.Error(),
-		})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	user, err := ctl.authService.Login(req.Email, req.Password)
 	if err != nil {
-		c.JSON(http.StatusUnauthorized, ApiResponse{
-			Success: false,
-			Message: "Login failed",
-			Error:   err.Error(),
-		})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, ApiResponse{
-		Success: true,
-		Message: "Login successful",
-		Data:    user,
-	})
+	token, err := services.GenerateJWT(user.ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
+
+func (ctl *Controller) Me(c *gin.Context) {
+	userID, exists := c.Get("userID")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "User ID not found in token claims"})
+		return
+	}
+	user, err := ctl.authService.GetUser(userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
