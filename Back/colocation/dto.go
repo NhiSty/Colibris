94273d04057273283e36019f9d7cf3a80a3e1044
Auralion <abdelhamidou4@gmail.com>
2024-05-28 11:14:37 +0200

    updated depencendes of database

--- Back/colocation/dto.go
+++ Back/colocation/dto.go
@@ -4,7 +4,7 @@ type ColocationCreateRequest struct {
 	Name        string `json:"name" binding:"required"`
 	UserId      uint   `json:"userId" binding:"required"`
 	Description string `json:"description"`
-	IsPermanent bool   `json:"isPermanent" binding:"required"`
+	IsPermanent bool   `json:"isPermanent"`
 	Address     string `json:"address" binding:"required"`
 	City        string `json:"city" binding:"required"`
 	ZipCode     string `json:"zipCode" binding:"required"`
