7fbb01f8212b094717b499a121d43df7a4db463f
Auralion <abdelhamidou4@gmail.com>
2024-06-16 20:58:59 +0200

    chore: Update security group rules for VM access

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,9 +13,9 @@ resource "aws_security_group" "security_group_vm" {
   name = var.security_group_name
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -26,12 +26,8 @@ resource "aws_security_group" "security_group_vm" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
+
 }
 
 resource "aws_instance" "vm" {
