78dbf4dedd1fa222531a2b17d143cf45875557fc
Auralion <abdelhamidou4@gmail.com>
2024-07-05 16:07:06 +0200

    test integration

--- Back/tests/colocation_test.go
+++ Back/tests/colocation_test.go
@@ -33,6 +33,30 @@ func TestCreateColocationSuccess(t *testing.T) {
 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
 }
 
+func TestCreateColocationSuccess2(t *testing.T) {
+
+	token := LogAsUser()
+	payload := dto.ColocationCreateRequest{
+		Name:        "Colocation 1",
+		Description: "Description of colocation 1",
+		Latitude:    48.8566,
+		Longitude:   2.3522,
+		Location:    "Paris",
+		IsPermanent: true,
+		UserId:      2,
+	}
+
+	jsonPayload, _ := json.Marshal(payload)
+	req, _ := http.NewRequest("POST", baseURL+"/colocations", bytes.NewBuffer(jsonPayload))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+}
+
 func TestGetColocationSuccess(t *testing.T) {
 	token := LogAsUser()
 	req, _ := http.NewRequest("GET", baseURL+"/colocations/1", nil)
@@ -111,4 +135,28 @@ func TestUpdateColocationForbidden(t *testing.T) {
 	resp, err := client.Do(req)
 	assert.Nil(t, err)
 	assert.Equal(t, http.StatusForbidden, resp.StatusCode)
+}
+
+func TestDeleteColocationSuccess(t *testing.T) {
+	token := LogAsUser()
+	req, _ := http.NewRequest("DELETE", baseURL+"/colocations/2", nil)
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+
+}
+
+func TestDeleteColocationFailure(t *testing.T) {
+	token := LogAsUser()
+	req, _ := http.NewRequest("DELETE", baseURL+"/colocations/100", nil)
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+
 }
--- Back/tests/register_test.go
+++ Back/tests/register_test.go
@@ -28,6 +28,28 @@ func TestRegisterSuccess(t *testing.T) {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+}
+
+func TestRegister2Success(t *testing.T) {
+	payload := dto.UserRegistrationRequest{
+		Email:     "test3@gmail.com",
+		Password:  "test123!",
+		FirstName: "user",
+		LastName:  "user",
+	}
+
+	jsonPayload, _ := json.Marshal(payload)
+	req, _ := http.NewRequest("POST", baseURL+"/auth/register", bytes.NewBuffer(jsonPayload))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
 }
 
