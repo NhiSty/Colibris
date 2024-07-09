028cfa57ae9756ee2ef0eec6b432135f9341eb79
Ossama9 <oussama.dahbi98@gmail.com>
2024-07-09 13:43:29 +0200

    front ok

--- Front/lib/main_website.dart
+++ Front/lib/main_website.dart
@@ -8,9 +8,11 @@ import 'package:front/services/user_service.dart';
 import 'package:front/website/pages/auth/login_page.dart';
 import 'package:front/website/pages/backoffice/colocation_handle_page.dart';
 import 'package:front/website/pages/backoffice/colocations/bloc/colocation_bloc.dart';
+import 'package:front/website/pages/backoffice/conversation_handle_page.dart';
 import 'package:front/website/pages/backoffice/feature_toggle_page.dart';
 import 'package:front/website/pages/backoffice/log_handle_page.dart';
 import 'package:front/website/pages/backoffice/logs/bloc/log_bloc.dart';
+import 'package:front/website/pages/backoffice/messages_handle_page.dart';
 import 'package:front/website/pages/backoffice/user/bloc/user_bloc.dart';
 import 'package:front/website/pages/backoffice/user_handle_page.dart';
 import 'package:front/website/pages/home_page.dart';
@@ -38,16 +40,16 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider<UserBloc>(
           create: (context) =>
-              UserBloc(userService: UserService())..add(LoadUsers()),
+          UserBloc(userService: UserService())..add(LoadUsers()),
         ),
         BlocProvider<LogBloc>(
           create: (context) =>
-              LogBloc(logService: LogService())..add(FetchLogs()),
+          LogBloc(logService: LogService())..add(FetchLogs()),
         ),
         BlocProvider<ColocationBloc>(
           create: (context) =>
-              ColocationBloc(colocationService: ColocationService())
-                ..add(LoadColocations()),
+          ColocationBloc(colocationService: ColocationService())
+            ..add(LoadColocations()),
         ),
       ],
       child: MaterialApp(
@@ -61,13 +63,36 @@ class MyApp extends StatelessWidget {
         supportedLocales: context.supportedLocales,
         locale: context.locale,
         initialRoute: '/login',
-        routes: {
-          '/login': (context) => const LoginPage(),
-          '/home': (context) => const HomePage(),
-          '/backoffice/user': (context) => const UserHandlePage(),
-          '/backoffice/logs': (context) => const LogPage(),
-          '/backoffice/colocations': (context) => const ColocationHandlePage(),
-          '/feature-flipping': (context) => const FeatureTogglePage(),
+        onGenerateRoute: (settings) {
+          print('Generating route for: ${settings.name}');
+          final Uri uri = Uri.parse(settings.name ?? '');
+          if (uri.pathSegments.length == 4 &&
+              uri.pathSegments[0] == 'backoffice' &&
+              uri.pathSegments[1] == 'colocations' &&
+              uri.pathSegments[3] == 'messages') {
+            final colocationId = uri.pathSegments[2];
+            return MaterialPageRoute(
+              builder: (context) => MessagesHandlePage(colocationId: int.parse(colocationId)),
+            );
+          }
+          switch (settings.name) {
+            case '/login':
+              return MaterialPageRoute(builder: (context) => const LoginPage());
+            case '/home':
+              return MaterialPageRoute(builder: (context) => const HomePage());
+            case '/backoffice/user':
+              return MaterialPageRoute(builder: (context) => const UserHandlePage());
+            case '/backoffice/logs':
+              return MaterialPageRoute(builder: (context) => const LogPage());
+            case '/backoffice/colocations':
+              return MaterialPageRoute(builder: (context) => const ColocationHandlePage());
+            case '/backoffice/messages':
+              return MaterialPageRoute(builder: (context) => const ConversationHandlePage());
+            case '/feature-flipping':
+              return MaterialPageRoute(builder: (context) => const FeatureTogglePage());
+            default:
+              return MaterialPageRoute(builder: (context) => const HomePage());
+          }
         },
       ),
     );
--- Front/lib/website/pages/home_page.dart
+++ Front/lib/website/pages/home_page.dart
@@ -128,7 +128,7 @@ class _HomePageState extends State<HomePage> {
               '/coloc_member_administration',
               'backoffice_homepage_coloc_members_description'.tr(),
               Icons.group),
-          _buildCard(context, 'backoffice_homepage_messages'.tr(), '/messages',
+          _buildCard(context, 'backoffice_homepage_messages'.tr(), '/backoffice/messages',
               'backoffice_homepage_messages_description'.tr(), Icons.build),
           _buildCard(context, 'backoffice_homepage_tasks'.tr(), '/tasks',
               'backoffice_homepage_tasks_description'.tr(), Icons.check),
