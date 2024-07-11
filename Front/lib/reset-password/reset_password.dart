3d6a4629cd71b9edf3ca86d98cd0d606b268dee6
Serkan Deveci <serkand27@hotmail.com>
2024-07-11 21:52:47 +0200

    style register and forget pwd

--- Front/lib/reset-password/reset_password.dart
+++ Front/lib/reset-password/reset_password.dart
@@ -2,7 +2,9 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:front/reset-password/reset_password_bloc.dart';
+import 'package:front/shared.widget/snack_bar_feedback_handling.dart';
 import 'package:front/validators.dart';
+import 'package:front/main.dart';
 
 class ResetPasswordScreen extends StatefulWidget {
   const ResetPasswordScreen({super.key});
@@ -22,125 +24,137 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => ResetPasswordBloc(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('forget_password_title'.tr()),
-          backgroundColor: Colors.green,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: BlocConsumer<ResetPasswordBloc, ResetPasswordState>(
-                listener: (context, state) {
-                  if (state is ResetPasswordError) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text(state.message)),
-                    );
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, '/login', (route) => false);
-                  } else if (state is ResetPasswordEmailSent) {
-                    setState(() {
-                      _isEmailSent = true;
-                    });
-                  } else if (state is ResetPasswordCodeVerified) {
-                    Navigator.pushNamed(context, '/reset-password-form',
-                        arguments: state.code);
-                  }
-                },
-                builder: (context, state) {
-                  if (state is ResetPasswordLoading) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Text(
-                          'Colibri',
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold),
+      child: GradientBackground(
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: Text('forget_password_title'.tr()),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: BlocConsumer<ResetPasswordBloc, ResetPasswordState>(
+                  listener: (context, state) {
+                    if (state is ResetPasswordError) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        showSnackBarFeedback(
+                          state.message,
+                          Colors.red,
                         ),
-                        Image.asset(
-                          'assets/images/login_page.png',
-                          height: 200,
-                          width: 200,
-                        ),
-                        const SizedBox(height: 20),
-                        if (state is ResetPasswordInitial ||
-                            state is ResetPasswordEmailSent) ...[
-                          Text(
-                            'forget_password_text'.tr(),
-                            style: const TextStyle(fontSize: 14),
+                      );
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, '/login', (route) => false);
+                    } else if (state is ResetPasswordEmailSent) {
+                      setState(() {
+                        _isEmailSent = true;
+                      });
+                    } else if (state is ResetPasswordCodeVerified) {
+                      Navigator.pushNamed(context, '/reset-password-form',
+                          arguments: state.code);
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is ResetPasswordLoading) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'Colibri',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          Image.asset(
+                            'assets/images/login_page.png',
+                            height: 200,
+                            width: 200,
                           ),
                           const SizedBox(height: 20),
-                          buildTextFormField(
-                            _emailController,
-                            Validators.validateEmail,
-                            'forget_password_email'.tr(),
-                            enabled: !_isEmailSent,
-                          ),
-                          const SizedBox(height: 20),
-                          ElevatedButton(
-                            onPressed: _isEmailSent
-                                ? null
-                                : () {
-                                    if (_formKey.currentState!.validate()) {
-                                      context.read<ResetPasswordBloc>().add(
-                                            SendResetEmail(
-                                                _emailController.text.trim()),
-                                          );
-                                    }
-                                  },
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.white,
-                              backgroundColor: Colors.green,
+                          if (state is ResetPasswordInitial ||
+                              state is ResetPasswordEmailSent) ...[
+                            Text(
+                              'forget_password_text'.tr(),
+                              style: const TextStyle(
+                                  fontSize: 14, color: Colors.white),
                             ),
-                            child: Text('forget_password_submit'.tr()),
-                          ),
-                        ],
-                        if (state is ResetPasswordEmailSent) ...[
-                          const SizedBox(height: 20),
-                          Text(
-                            'forget_password_received_code_by_mail'.tr(),
-                            style: const TextStyle(fontSize: 14),
-                          ),
-                          const SizedBox(height: 20),
-                          buildTextFormField(
-                            _codeController,
-                            Validators.validateEmailCode,
-                            'forget_password_code'.tr(),
-                          ),
-                          const SizedBox(height: 20),
-                          ElevatedButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                context.read<ResetPasswordBloc>().add(
-                                      VerifyResetCode(
-                                          _codeController.text.trim()),
-                                    );
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text(
-                                        'forget_password_code_invalid'.tr()),
-                                  ),
-                                );
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.white,
-                              backgroundColor: Colors.green,
+                            const SizedBox(height: 20),
+                            buildTextFormField(
+                              _emailController,
+                              Validators.validateEmail,
+                              'forget_password_email'.tr(),
+                              enabled: !_isEmailSent,
                             ),
-                            child: Text('forget_password_submit_code'.tr()),
-                          ),
+                            const SizedBox(height: 20),
+                            ElevatedButton(
+                              onPressed: _isEmailSent
+                                  ? null
+                                  : () {
+                                if (_formKey.currentState!.validate()) {
+                                  context.read<ResetPasswordBloc>().add(
+                                    SendResetEmail(
+                                        _emailController.text
+                                            .trim()),
+                                  );
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor: Colors.blueGrey,
+                              ),
+                              child: Text('forget_password_submit'.tr()),
+                            ),
+                          ],
+                          if (state is ResetPasswordEmailSent) ...[
+                            const SizedBox(height: 20),
+                            Text(
+                              'forget_password_received_code_by_mail'.tr(),
+                              style: const TextStyle(
+                                  fontSize: 14, color: Colors.white),
+                            ),
+                            const SizedBox(height: 20),
+                            buildTextFormField(
+                              _codeController,
+                              Validators.validateEmailCode,
+                              'forget_password_code'.tr(),
+                            ),
+                            const SizedBox(height: 20),
+                            ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  context.read<ResetPasswordBloc>().add(
+                                    VerifyResetCode(
+                                        _codeController.text.trim()),
+                                  );
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    showSnackBarFeedback(
+                                      'forget_password_code_invalid'.tr(),
+                                      Colors.red,
+                                    ),
+                                  );
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor: Colors.blueGrey,
+                              ),
+                              child: Text('forget_password_submit_code'.tr()),
+                            ),
+                          ],
                         ],
-                      ],
-                    );
-                  }
-                },
+                      );
+                    }
+                  },
+                ),
               ),
             ),
           ),
@@ -150,19 +164,28 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   }
 
   TextFormField buildTextFormField(
-    TextEditingController controller,
-    String? Function(String?)? validator,
-    String label, {
-    TextInputType keyboardType = TextInputType.text,
-    bool obscureText = false,
-    bool enabled = true,
-  }) {
+      TextEditingController controller,
+      String? Function(String?)? validator,
+      String label, {
+        TextInputType keyboardType = TextInputType.text,
+        bool obscureText = false,
+        bool enabled = true,
+      }) {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
         labelText: label,
+        labelStyle: const TextStyle(color: Colors.white),
         border: const OutlineInputBorder(),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.blueGrey),
+        ),
+        errorStyle: TextStyle(color: Colors.red[200]),
       ),
+      style: const TextStyle(color: Colors.white),
       keyboardType: keyboardType,
       validator: validator,
       obscureText: obscureText,
--- Front/lib/reset-password/reset_password_form.dart
+++ Front/lib/reset-password/reset_password_form.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:front/reset-password/reset_password_bloc.dart';
+import 'package:front/main.dart';
 
 class ResetPasswordFormScreen extends StatelessWidget {
   ResetPasswordFormScreen({super.key});
@@ -9,25 +10,22 @@ class ResetPasswordFormScreen extends StatelessWidget {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _passwordConfirmController =
-      TextEditingController();
+  TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final String emailCode =
-        ModalRoute.of(context)?.settings.arguments as String;
+    ModalRoute.of(context)?.settings.arguments as String;
 
     return BlocProvider(
       create: (_) => ResetPasswordBloc(),
-      child: PopScope(
-        canPop: false,
-        onPopInvoked: (didPop) async {
-          Navigator.pushNamedAndRemoveUntil(
-              context, '/login', (route) => false);
-        },
+      child: GradientBackground(
         child: Scaffold(
+          backgroundColor: Colors.transparent,
           appBar: AppBar(
             title: Text('forget_password_change_title'.tr()),
-            backgroundColor: Colors.green,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
             leading: IconButton(
               icon: const Icon(Icons.arrow_back),
               onPressed: () =>
@@ -58,27 +56,23 @@ class ResetPasswordFormScreen extends StatelessWidget {
                       return Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
-                          const Text(
-                            'Colibri',
-                            style: TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
                           Image.asset(
                             'assets/images/login_page.png',
                             height: 200,
                             width: 200,
                           ),
-                          Text(
-                            'forget_password_change_text'.tr(),
-                            style: const TextStyle(fontSize: 16),
+                          const SizedBox(height: 20),
+                          buildTextFormField(
+                            _passwordController,
+                            'forget_password_change_new_password'.tr(),
+                            obscureText: true,
                           ),
-                          buildTextFormField(_passwordController,
-                              'forget_password_change_new_password'.tr(),
-                              obscureText: true),
                           const SizedBox(height: 10),
-                          buildTextFormField(_passwordConfirmController,
-                              'forget_password_change_confirm_password'.tr(),
-                              obscureText: true),
+                          buildTextFormField(
+                            _passwordConfirmController,
+                            'forget_password_change_confirm_password'.tr(),
+                            obscureText: true,
+                          ),
                           const SizedBox(height: 20),
                           ElevatedButton(
                             onPressed: () {
@@ -88,9 +82,10 @@ class ResetPasswordFormScreen extends StatelessWidget {
                             },
                             style: ElevatedButton.styleFrom(
                               foregroundColor: Colors.white,
-                              backgroundColor: Colors.green,
+                              backgroundColor: Colors.blueGrey,
                             ),
-                            child: Text('forget_password_change_submit'.tr()),
+                            child:
+                            Text('forget_password_change_submit'.tr()),
                           ),
                         ],
                       );
@@ -106,24 +101,33 @@ class ResetPasswordFormScreen extends StatelessWidget {
   }
 
   TextFormField buildTextFormField(
-    TextEditingController controller,
-    String label, {
-    TextInputType keyboardType = TextInputType.text,
-    bool obscureText = false,
-  }) {
+      TextEditingController controller,
+      String label, {
+        TextInputType keyboardType = TextInputType.text,
+        bool obscureText = false,
+      }) {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
         labelText: label,
+        labelStyle: const TextStyle(color: Colors.white),
         border: const OutlineInputBorder(),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.blueGrey),
+        ),
       ),
+      style: const TextStyle(color: Colors.white),
       keyboardType: keyboardType,
       obscureText: obscureText,
       validator: (value) {
         if (value == null || value.isEmpty) {
           return '$label invalide';
         }
-        if (label == 'Confirm password' && value != _passwordController.text) {
+        if (label == 'forget_password_change_confirm_password'.tr() &&
+            value != _passwordController.text) {
           return 'forget_password_pwd_not_match'.tr();
         }
         return null;
@@ -133,10 +137,10 @@ class ResetPasswordFormScreen extends StatelessWidget {
 
   void _resetPassword(BuildContext context, String emailCode) {
     context.read<ResetPasswordBloc>().add(
-          ResetPasswordWithEmailCode(
-            email: _passwordController.text.trim(),
-            code: emailCode,
-          ),
-        );
+      ResetPasswordWithEmailCode(
+        email: _passwordController.text.trim(),
+        code: emailCode,
+      ),
+    );
   }
 }
