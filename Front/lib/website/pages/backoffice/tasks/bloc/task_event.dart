142b063cee986db724050b9ce24a422c5a4213f0
Thomas J <thomas.jallu@gmail.com>
2024-07-12 04:23:40 +0200

    Add bloc in website for tasks

---
+++ Front/lib/website/pages/backoffice/tasks/bloc/task_event.dart
@@ -0,0 +1,63 @@
+part of 'task_bloc.dart';
+
+@immutable
+sealed class TaskEvent {}
+
+class LoadTasks extends TaskEvent {
+  final int page;
+  final int pageSize;
+
+  LoadTasks({this.page = 1, this.pageSize = 5});
+}
+
+class SearchTasks extends TaskEvent {
+  final String query;
+
+  SearchTasks({required this.query});
+}
+
+class ClearSearch extends TaskEvent {}
+
+class AddTask extends TaskEvent {
+  final String title;
+  final String description;
+  final String date;
+  final int duration;
+  final String picture;
+  final int colocationId;
+
+  AddTask({
+    required this.title,
+    this.description = '',
+    required this.date,
+    required this.duration,
+    this.picture = '',
+    required this.colocationId,
+  });
+}
+
+class EditTask extends TaskEvent {
+  final int taskId;
+  final String title;
+  final String description;
+  final String date;
+  final int duration;
+  final String picture;
+  final int colocationId;
+
+  EditTask({
+    required this.taskId,
+    required this.title,
+    this.description = '',
+    required this.date,
+    required this.duration,
+    this.picture = '',
+    required this.colocationId,
+  });
+}
+
+class DeleteTask extends TaskEvent {
+  final int id;
+
+  DeleteTask({required this.id});
+}
